128797b24a3f4eb2edf80e67d50e9aa01c111cbb
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-11 14:37:58 +0100

    modified

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,6 +3,23 @@ echo "FLIP COIN COMBINATION PROBLEM"
 declare -A dict
 countT=0
 countH=0
+for ((i=1;i<=5;i++))
+do
+	rand=$((RANDOM%2))
+	case $rand in
+		0)dict["flip $i"]=H
+		((countH++));;
+		1)dict["flip $i"]=T
+		((countT++));;
+		*);; 
+	esac
+done
+Hper=`echo $countH |awk '{print (100*$1)/5}'`
+Tper=`echo  $countT |awk '{print (100*$1)/5}'`
+echo ${!dict[@]}:${dict[@]}
+echo "HEAD %:$Hper% TAIL %:$Tper%"
+countH=0
+countT=0
 for ((i=1;i<=10;i++))
 do
 	rand=$((RANDOM%4))
