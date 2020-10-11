5e90fd29907b2f0ff318f856816514403d2ef74c
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-11 14:21:56 +0100

    Added:store Dublet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,20 +1,23 @@
 #!/bin/bash -x
 echo "FLIP COIN COMBINATION PROBLEM"
 declare -A dict
-countH=0
 countT=0
-for ((i=1;i<=5;i++))
+countH=0
+for ((i=1;i<=10;i++))
 do
-	rand=$((RANDOM%2))
+	rand=$((RANDOM%4))
 	case $rand in
-		0)dict["flip $i"]=H
+		0)dict["flip $i"]=HH
 		((countH++));;
-		1)dict["flip $i"]=T
+		1)dict["flip $i"]=TT
 		((countT++));;
+		2)dict["flip $i"]=TH;;
+		3)dict["flip $i"]=HT;;
 		*);; 
 	esac
 done
 Hper=`echo $countH |awk '{print (100*$1)/5}'`
 Tper=`echo  $countT |awk '{print (100*$1)/5}'`
+echo "$(($i-1)) Flips"
 echo ${!dict[@]}:${dict[@]}
-echo "HEAD %:$Hper% TAIL %:$Tper%"
+echo "HH %:$Hper% TT %:$Tper%"
