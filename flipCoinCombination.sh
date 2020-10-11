e63056707b51e1356d5dc9408af6c33094d58fa8
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-11 22:35:47 +0100

    added:sorting and display winner

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,31 +3,41 @@ echo "FLIP COIN COMBINATION PROBLEM"
 declare -A dict
 countT=0
 countH=0
+j=0
 for ((i=1;i<=5;i++))
 do
 	rand=$((RANDOM%2))
 	case $rand in
 		0)dict["flip $i"]=H
-		((countH++));;
+		((countH++))
+		sort[j]=H
+		((j++));;
 		1)dict["flip $i"]=T
-		((countT++));;
+		((countT++))
+		sort[j]=T
+		((j++));;
 		*);; 
 	esac
 done
+echo "5 flips"
 Hper=`echo $countH |awk '{print (100*$1)/5}'`
 Tper=`echo  $countT |awk '{print (100*$1)/5}'`
 echo ${!dict[@]}:${dict[@]}
 echo "HEAD %:$Hper% TAIL %:$Tper%"
-countH=0
-countT=0
+DcountH=0
+DcountT=0
 for ((i=1;i<=10;i++))
 do
 	rand=$((RANDOM%4))
 	case $rand in
 		0)dict["flip $i"]=HH
-		((countH++));;
+		((DcountH++))
+		 sort[j]=HH
+                ((j++));;
 		1)dict["flip $i"]=TT
-		((countT++));;
+		((DcountT++))
+		 sort[j]=TT
+                ((j++));;
 		2)dict["flip $i"]=TH;;
 		3)dict["flip $i"]=HT;;
 		*);; 
@@ -38,16 +48,20 @@ Tper=`echo  $countT |awk '{print (100*$1)/10}'`
 echo "$(($i-1)) Flips"
 echo ${!dict[@]}:${dict[@]}
 echo "HH %:$Hper% TT %:$Tper%"
-countT=0
-countH=0
+TcountT=0
+TcountH=0
 for ((i=1;i<=10;i++))
 do
         rand=$((RANDOM%4))
         case $rand in
                 0)dict["flip $i"]=HHH
-                ((countH++));;
+                ((TcountH++))
+		 sort[j]=HHH
+                ((j++));;
                 1)dict["flip $i"]=TTT
-                ((countT++));;
+                ((TcountT++))
+		sort[j]=TTT
+                ((j++));;
                 2)dict["flip $i"]=TTH;;
                 3)dict["flip $i"]=THT;;
                 4)dict["flip $i"]=HTT;;
@@ -62,3 +76,25 @@ Tper=`echo  $countT |awk '{print (100*$1)/10}'`
 echo "$(($i-1)) Flips"
 echo ${!dict[@]}:${dict[@]}
 echo "HHH %:$Hper% TTT %:$Tper%"
+echo "COMBINATIONS:${sort[@]}"
+max=$countH
+for k in {$countT,$DcountH,$DcountT,$TcountH,$TcountT}
+do
+	if [ $k -gt $max ]
+	then 
+		max=$k
+	else
+		continue
+	fi
+done
+case $max in
+ 	$countH) echo "H WIN";;
+	$countT) echo "T WIN";;
+	$DcountH) echo "HH WIN";;
+	$DcountT) echo "TT WIN";;
+	$TcountH) echo "HHH WIN";;
+	$TcountT) echo "TTT WIN";;
+	*);;
+esac
+
+
