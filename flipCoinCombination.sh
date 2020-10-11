3d674cede78bd864d28b623c417c6482847e1ec7
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-11 10:25:35 +0100

    Added:Store Singlet result to Dictianry

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -x
 echo "FLIP COIN COMBINATION PROBLEM"
+declare -A dict
+countH=0
+countT=0
+for ((i=1;i<=5;i++))
+do
+	rand=$((RANDOM%2))
+	case $rand in
+		0)dict["flip $i"]=H
+		((countH++));;
+		1)dict["flip $i"]=T
+		((countT++));;
+		*);; 
+	esac
+done
+Hper=`echo $countH |awk '{print (100*$1)/5}'`
+Tper=`echo  $countT |awk '{print (100*$1)/5}'`
+echo ${!dict[@]}:${dict[@]}
+echo "HEAD %:$Hper% TAIL %:$Tper%"
